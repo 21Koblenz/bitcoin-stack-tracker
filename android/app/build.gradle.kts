c86abc5cd69aff37642b08d4254456ec74d3d6e2
Einundzwanzig Koblenz <koblenzeinundzwanzig@proton.me>
2026-08-19 05:44:57 +0200

    Android: target API 36 for device compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "de.einundzwanzigkoblenz.bitcoinstacktracker"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "de.einundzwanzigkoblenz.bitcoinstacktracker"
         minSdk = 28
-        targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0-alpha01"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "0.1.0-alpha02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
